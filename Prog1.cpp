a5a841909b4712012dfacfdfac24783af04211f4
RicardoJafet <105650735+RicardoJafet@users.noreply.github.com>
2022-05-19 17:55:18 -0500

    Hola Mundo
    Programa que muestra un mensaje de saludo enfocado al inicio del mundo de la programación durante el curso de Programación Estructurada.

---
+++ Prog1.cpp
@@ -0,0 +1,10 @@
+/*Autor: Ricardo Jafet Granados Chable, Realizado: 03/02/2022
+	Primer programa en C, Hola Mundo
+	*/
+#include<stdio.h>
+
+int main() {
+	printf("Hola Mundo!\n");
+	printf("\nRicardo Jafet");
+	return 0;
+} 
